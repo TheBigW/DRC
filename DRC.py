210bc1b8e14ea8bacc0e28d705a32ec5a36bb20e
tobias <tobiaswenig@yahoo.com>
2015-03-31 17:38:14 +0200

    workaround try due to gstreamer limitations, improved filter loading during song play

--- DRC.py
+++ DRC.py
@@ -46,39 +46,44 @@ class DRCPlugin(GObject.Object, Peas.Activatable):
         super(DRCPlugin, self).__init__()
 
     def updateFilter(self, filterFileName, numChannels = None):
-        filter_array = DRCFileTool.LoadAudioFile(filterFileName, numChannels)
-        #pass the filter data to the fir filter
-        #print inspect.getdoc( self.fir_filter )
-        num_filter_coeff = len( filter_array )
-        if num_filter_coeff > 0:
-            self.fir_filter.set_property( 'latency', int(num_filter_coeff/2) )
-            print( "num_filter_coeff", num_filter_coeff )
-            kernel = []
-            #print( "kernel : ", kernel)
-            for i in range(0, num_filter_coeff):
-                kernel.append( filter_array[i] )
-                #print( filter_array[i] )
-            #TODO: check possibility to scale filter strength (configurable devisor etc...)
-            self.fir_filter.set_property('kernel', kernel)
+        try:
+            self.remove_Filter()
+            filter_array = DRCFileTool.LoadAudioFile(filterFileName, numChannels)
+            #pass the filter data to the fir filter
+            num_filter_coeff = len( filter_array )
+            if num_filter_coeff > 0:
+                self.fir_filter.set_property( 'latency', int(num_filter_coeff/2) )
+                print( "num_filter_coeff", num_filter_coeff )
+                kernel = self.fir_filter.get_property('kernel')
+                kernel = []
+                print( "kernel : ", kernel)
+                for i in range(0, num_filter_coeff):
+                    kernel.append( filter_array[i] )
+                    #print( filter_array[i] )
+                #TODO: check possibility to scale filter strength (configurable devisor etc...)
+                self.fir_filter.set_property('kernel', kernel)
+            self.set_filter()
+        except Exception as inst:
+            print( 'error updating filter',  sys.exc_info()[0], type(inst), inst )
+            pass
 
     def do_activate(self):
         try:
+            self.filterSet = False
             self.shell = self.object
             self.shell_player = self.shell.props.shell_player
             self.player = self.shell_player.props.player
-            #audioiirfilter
             self.fir_filter = Gst.ElementFactory.make('audiofirfilter', 'MyFIRFilter')
-            #print( "audiofirfilter :" + str(self.fir_filter) )
             #open filter files
             aCfg = DRCConfig()
             self.updateFilter(aCfg.filterFile, aCfg.numFilterChanels)
-            #print( inspect.getdoc( kernel ) )
-            self.set_filter()
-            print( "filter succesfully set" )
+            #print( str(dir( self.shell.props)) )
         except Exception as inst:
             print( 'filter not set',  sys.exc_info()[0], type(inst), inst )
             pass
         self.psc_id = self.shell_player.connect('playing-song-changed', self.playing_song_changed)
+        #no possible workaround as somehow never reaches end of song :(
+        #self.psc_id = self.shell_player.connect('elapsed-changed', self.elapsed_changed)
         #finally add UI
         self.add_ui( self, self.shell )
 
@@ -96,11 +101,22 @@ class DRCPlugin(GObject.Object, Peas.Activatable):
 
     def set_filter(self):
         try:
-            print( 'adding filter' )
-            self.player.add_filter(self.fir_filter)
-            print('done setting filter' )
+            if not self.filterSet:
+                print( 'adding filter' )
+                self.player.add_filter(self.fir_filter)
+                print('done setting filter' )
+            self.filterSet = True
         except Exception as inst:
             print( 'unexpected exception',  sys.exc_info()[0], type(inst), inst )
+            pass
+
+    def remove_Filter(self):
+        try:
+            if self.filterSet:
+                self.player.remove_filter(self.fir_filter)
+                print( 'filter disabled' )
+            self.filterSet = False
+        except:
             pass
 
     def do_deactivate(self):
@@ -108,9 +124,7 @@ class DRCPlugin(GObject.Object, Peas.Activatable):
         self.shell_player.disconnect(self.psc_id)
 
         try:
-            self.player.remove_filter(self.fir_filter)
-            print( 'filter disabled' )
-            shell = self.object
+            self.remove_Filter()
             self._appshell.cleanup()
         except:
             pass
@@ -119,13 +133,30 @@ class DRCPlugin(GObject.Object, Peas.Activatable):
         del self.shell
         del self.fir_filter
 
+    def elapsed_changed(self, sp, elapsed):
+        #workaround due to FIR filter issues during playing back multiple songs
+        #switching as well as new song select in UI seems to fix that
+        duration = sp.get_playing_song_duration()
+        #print("duration: " + str(duration))
+        #print("elapsed : " + str(elapsed) )
+        diff = duration - elapsed
+        #print("diff : " + str(diff))
+        if diff <= 1:
+            print("end of song reached : skip next")
+            #time.sleep(0.5)
+            sp.do_next()
+
     def playing_song_changed(self, sp, entry):
         if entry == None:
             return
-        genre = entry.get_string(RB.RhythmDBPropType.GENRE)
-        print( "genre : ", str(genre) )
-        #print( entry.get_string(RB.RhythmDBPropType.ALBUM) )
-        #print( entry.get_string(RB.RhythmDBPropType.TITLE) )
+        #workaround due to FIR filter issues during playing back multiple songs
+        #switching as well as new song select in UI seems to fix that
+        '''TODO: this will fail for manually selecting last song in list in UI
+            clean way would be to check has-prev/has-next but seems to be useless:
+            returns true even if no son is previous in current UI list...
+        '''
+        sp.do_next()
+        sp.do_previous()
 
     def find_file(self, filename):
         info = self.plugin_info
