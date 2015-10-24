a87eab6fa40111e002cc4defbfca8634d8331d3c
Tobias Wenig <tobiaswenig@yahoo.com>
2015-10-24 14:02:13 +0200

    adapted gst-plugins patch script for Ubuntu 15.10;gst plugins version configurable now in one line

--- gst-plugins-good-patch/patch-gstreamer.sh
+++ gst-plugins-good-patch/patch-gstreamer.sh
@@ -1,16 +1,18 @@
+GST_PLUGIN_VERSION=1.6.0
 apt-get source gstreamer1.0-plugins-good
 #sudo apt-get install auto-apt
 #sudo auto-apt update
 #sudo auto-apt updatedb && sudo auto-apt update-local
 sudo apt-get install libglib2.0-dev
+sudo apt-get install libglib2.0-dev
 sudo apt-get install libgstreamer1.0-dev
 sudo apt-get install libgstreamer-plugins-base1.0-dev
 sudo apt-get install liborc-0.4-dev
 sudo apt-get install automake
-cp *.c ./gst-plugins-good1.0-1.4.5/gst/audiofx
-cp *.h ./gst-plugins-good1.0-1.4.5/gst/audiofx
+cp *.c ./gst-plugins-good1.0-$GST_PLUGIN_VERSION/gst/audiofx
+cp *.h ./gst-plugins-good1.0-$GST_PLUGIN_VERSION/gst/audiofx
 #sudo auto-apt run ./gst-plugins-good1.0-1.4.5/configure
-./gst-plugins-good1.0-1.4.5/configure
-cd ./gst-plugins-good1.0-1.4.5/gst/audiofx
+cd ./gst-plugins-good1.0-$GST_PLUGIN_VERSION
+./configure
 sudo make && sudo make install && sudo cp /usr/local/lib/gstreamer-1.0/* /usr/lib/i386-linux-gnu/gstreamer-1.0/ 
 
